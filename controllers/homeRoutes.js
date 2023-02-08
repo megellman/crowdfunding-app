c5c28a96e69f069e453c95f614e4015f1ab2a856
Megan Ellman <meganlellman@gmail.com>
2023-02-08 13:29:02 -0800

    created route to render list of all projects from database

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { User, Project } = require('../../models');
+
+// / homepage route renders a list of all projects from the database
+router.get('/', async (req, res) => {
+    try {
+       const userData = await User.findAll({
+        include: [
+            {
+                model: Project,
+                attributes: ['name', 'description', 'needed_funding'],
+            },
+        ],
+       });
+       const data = userData.map((user) => user.get({ plain: true }))
+       res.render('homepage', {data});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
