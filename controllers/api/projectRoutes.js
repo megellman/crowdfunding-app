6b149439e6744c0b8c13fb47c8f32c2f543655f3
Megan Ellman <meganlellman@gmail.com>
2023-02-08 13:39:23 -0800

    add get route to render individual project

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -14,6 +14,23 @@ router.post('/', async (req, res) => {
   }
 });
 
+// renders an individual project's details based on the route parameter id.
+router.get('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id);
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project with this ID found!' });
+      return;
+    }
+
+    const project = projectData.get({ plain: true });
+    res.render('project', {project});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 router.delete('/:id', async (req, res) => {
   try {
     const projectData = await Project.destroy({
