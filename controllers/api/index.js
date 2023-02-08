6d164d0138a51895d89c67955b12054862d6a4db
Megan Ellman <meganlellman@gmail.com>
2023-02-08 14:02:43 -0800

    created utils to authenticate if user has logged in or not

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 const profileRoutes = require('./profileRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
-router.use('/profile', profileRoutes);
+router.use('/profile', withAuth, profileRoutes);
 
 module.exports = router;
--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Project } = require('../../models');
 
+
 // renders the logged-in user's projects and a form to create a new project
 router.get('/', async (req, res) => {
     try {
