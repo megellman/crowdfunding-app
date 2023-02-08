05b5426dd5fe935c304ae965dcd3e214a4c5d3fb
Megan Ellman <meganlellman@gmail.com>
2023-02-08 13:43:24 -0800

    created login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// renders a form to log in and a form to create a new account
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
